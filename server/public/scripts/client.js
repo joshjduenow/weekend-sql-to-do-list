005b1bbd40861bdacfe5151f045413d05561df1e
Joshua Duenow <joshjduenow@gmail.com>
2023-11-11 08:56:05 -0600

    added post route/function. able to add to sql table

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,13 +1,44 @@
 console.log('JS is sourced!');
 
+
+
+// GET TO-DO FUNCTION
 function getTodos() {
     axios({
         type: 'GET',
         url: '/todos'
     }).then(res => {
+    console.log('getTodos() response', response.data);
         renderTodos(res.data)
-    })
+    }).catch(function (error) {
+        console.log('error in GET', error);
+      });
 }
+
+// POST TO-DO FUNCTION
+function addToDo(event) {
+    let incToDo = {
+      name: document.getElementById('newToDo').value,
+    //   age: document.getElementById('ageIn').value,
+    //   gender: document.getElementById('genderIn').value,
+    //   transfer: document.getElementById('readyForTransferIn').value,
+    //   notes: document.getElementById('notesIn').value
+  
+  
+    }
+    console.log(incToDo);
+    axios({
+      method: 'POST',
+      url: '/todos',
+      data: incToDo,
+    }).then(function (response) {
+      console.log('incToDo()', response.data);
+      getTodos();
+    }).catch(function (error) {
+      console.log('Error in POST', error)
+      alert('Unable to add todo at this time. Please try again later.');
+    });
+  }
 
 function renderTodos(todos) {
     
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,7 +1,36 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
+const express = require('express');
 
+// GET ROUTE
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "todos";';
+    pool.query(queryText).then(result => {
+        console.log('in router.get',);
+        res.send(result.rows);
+    })
+        .catch(error => {
+            console.log('error with todo list', error);
+            res.sendStatus(500);
+        });
+});
 
+// POST ROUTE
+router.post('/', (req, res) => {
+    let newToDo = req.body;
+    console.log('Adding TO-DO', newToDo);
+
+    let queryText = `INSERT INTO "todos" ("text")
+                     VALUES ($1);`;
+    pool.query(queryText, [newToDo.name])
+        .then(result => {
+            res.sendStatus(201);
+        })
+        .catch(error => {
+            console.log('Error adding new todo', error);
+            res.sendStatus(500);
+        });
+});
 
 
 module.exports = router;
