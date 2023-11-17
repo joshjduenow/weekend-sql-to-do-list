39ae0cbd68d984fc7e2c33a3250115777fd23ce5
Joshua Duenow <joshjduenow@gmail.com>
2023-11-17 08:54:28 -0600

    changed the server.js port

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const todos = require('./routes/todos.router.js');
 
-let PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001;
 
 // Do not modify this!
 if (process.env.NODE_ENV == 'test') {
